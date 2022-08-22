22df89056b5bc66007490d008da3ffd06c27949b
imran <87297004+imranshaikh9930@users.noreply.github.com>
2022-08-22 23:50:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// Calculate Button
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+
+    let billAmount = document.getElementById("bill_amount").value;
+    let tipPercent = document.getElementById("tip_P").value;
+    let tipAmount = document.getElementById("tip_amount").value = (billAmount/100) * tipPercent;
+    document.getElementById('total-Bill').value = parseFloat(billAmount) + parseFloat(tipAmount);
+    
+})
+// Reset Buttion
+
+let reset = document.getElementById("reset");
+let inputElement = document.querySelectorAll("input");
+
+reset.addEventListener("click", function () {
+    inputElement.forEach(input => input.value = " ");
+ 
+    
+})
+
+
+
+
+
+    
